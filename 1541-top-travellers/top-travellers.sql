fb22551a61dc3d48122acb57f0c2a9324ffc23b5
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2023-11-12 22:03:42 +0530

    Time: 1624 ms (50.14%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -4,7 +4,3 @@ FROM Users as u
 LEFT JOIN Rides as r ON u.id = r.user_id
 GROUP BY r.user_id
 ORDER BY travelled_distance DESC, name ASC
-
-# UPDATE travelled_distance
-# SET travelled_distance = 0
-# WHERE travelled_distance IS NULL
