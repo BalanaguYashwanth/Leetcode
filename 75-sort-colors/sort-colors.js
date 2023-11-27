44224cc2e1d06644ca560605c0d02ae85b8ec4af
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2023-11-27 12:18:55 +0530

    Time: 52 ms (58.55%) | Memory: 42 MB (48.45%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,25 +3,34 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    const sort=(i,j,arr)=>{
-        if(i<arr.length && j<arr.length){
-            if(arr[i] > arr[j]){
-                temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
-            }
-            j = j + 1
-            return sort(i,j,arr)
-        }else{
-            if(i<arr.length){
-                console.log('other---->',i,j,arr)
+    let l = 0
+    let r = nums.length-1
+    let i = 0
+
+    const swap = (start,end) => {
+        const temp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = temp
+    }
+    const other = nums.includes(0) ? 0 : 1 
+    while(i<=r){
+        if(nums[i]==2){
+            swap(i,r)
+            r = r-1
+        }
+        if(nums[i]==1){
+            i = i+1
+        }
+        if(nums[i]==(0)){
+            if(i==l){
                 i = i+1
-                j = i+1
-                return sort(i,j,arr)
+                l = l+1
+            }else{
+                swap(l,i)
+                i = i+1
+                l = l+1
             }
         }
     }
-    let i=0;
-    let j=1
-    sort(i,j,nums)
+    return nums
 };
