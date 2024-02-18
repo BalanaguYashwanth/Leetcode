0f48b90b360ed9429c7db1a4afee867c23e581d8
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-02-18 15:15:39 +0530

    Time: 101 ms (35.53%) | Memory: 52.5 MB (48.16%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -7,28 +7,12 @@ var maximumProduct = function(nums) {
     let negSum = 1
     let povSum = 1
     const povFn = () => {
-        nums.sort((a,b)=>b-a)
-        negSum = negSum * nums[0]
-        for(let i=0; i<nums.length; i++){
-            povSum = povSum * nums[i]
-            if(i==2){
-                break
-            }
-        }
+        povSum = nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]
     }
 
     const negFn = () => {
         nums.sort((a,b)=>a-b)
-        for(let i=0; i<nums.length; i++){
-            if(i==2){
-                break
-            }
-            if(nums[i]>0){
-                negSum = 1
-                break
-            }
-            negSum = negSum * nums[i]
-        }
+        negSum = nums[0]*nums[1]*nums[nums.length-1]
     }
 
     if(nums.length==3){
