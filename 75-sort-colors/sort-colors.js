bcb811043d2af8ec1f6b3ddd6803f154014fe143
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-03-08 21:01:34 +0530

    Time: 53 ms (57.00%) | Memory: 48.7 MB (59.91%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,43 +3,28 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let end = nums.length-1
     let initial = 0
-    if(nums.length<=1){
-        return;
-    }
+    let end = nums.length-1
     let i = 0
-    while(i<nums.length){
-        if(nums[i] === 2){
-           
-            if(i===nums.length-1){
-                return;
-            }
-            if(nums[end] === 2){
-                end = end - 1
-                continue
-            }
-           
-            if(i>end){
-                return;
-            }
-            const temp = nums[i]
-            nums[i] = nums[end]
-            nums[end] = temp
-            end = end - 1
-            console.log('end---',end,'i---',i,'nums---', nums)
-        }
+
+    const swap = (leftIndex, rightIndex) => {
+        const temp = nums[leftIndex]
+        nums[leftIndex] = nums[rightIndex]
+        nums[rightIndex] = temp
+    }
+
+    while(i<=end){
         if(nums[i]==0){
-            if((nums[initial] != nums[i]) && (initial!=i)){
-                const temp = nums[initial]
-                nums[initial] = nums[i]
-                nums[i] = temp
-            }
+            swap(initial, i)
             initial = initial+1
+            i = i+1
         }
-        i = i+1
-        // if(i>=end){
-        //     break
-        // }
+        else if(nums[i]==1){
+            i = i+1
+        }
+        else{
+            swap(i,end)
+            end = end-1
+        }
     }
 };
