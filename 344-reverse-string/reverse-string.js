cdf8ccbe96739256aefd17f6ad85d299c5f69c4a
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-05-08 10:49:09 +0530

    Time: 82 ms (36.91%) | Memory: 59.1 MB (5.17%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,19 +3,23 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
+
+    const swap  = (l,r) => {
+        let temp = s[r]
+        s[r] = s[l]
+        s[l] = temp
+    }
     
-    const getReverse = (l,r) => {
+    const reverseHelper = (l,r) => {
         if(l==r){
             return s
         }
         if(l>r){
             return s
         }
-        let temp = s[r]
-        s[r] = s[l]
-        s[l] = temp
-        return getReverse(l+1, r-1)
+        swap(l,r)
+        return reverseHelper(l+1, r-1)
     }
 
-    return getReverse(0, s.length-1)
+    return reverseHelper(0, s.length-1)
 };
