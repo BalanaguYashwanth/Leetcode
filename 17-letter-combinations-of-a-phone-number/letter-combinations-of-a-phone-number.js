9df9ec1b5ccceef102db0eb8d6b6161d4c8cd0f5
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-07-08 10:43:42 +0530

    Time: 66 ms (5.89%) | Memory: 49.2 MB (14.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -3,57 +3,40 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    const arr = {
-        '2':['a','b','c'], 
-        '3':['d','e','f'], 
-        '4': ['g','h','i'],
-        '5': ['j', 'k', 'l'],
-        '6':['m', 'n', 'o'],
-        '7':['p', 'q', 'r', 's'],
-        '8':['t', 'u', 'v'],
-        '9':['w','x','y','z']
+    const phoneNumbers = {
+        '2': 'abc', 
+        '3': 'def', 
+        '4': 'ghi', 
+        '5': 'jkl',
+        '6': 'mno', 
+        '7': 'pqrs', 
+        '8': 'tuv', 
+        '9': 'wxyz'
+    }
+    const resCombinations = []
+    const getCombinations = (totalArr, index, letter) => {
+        if(index === totalArr.length){
+            resCombinations.push(letter)
+            return
         }
-    const result = []
-
-    const addElements = (element, totalArr, next) => {
-        for(let i of totalArr[next]){
-            const temp = element + i
-            result.push(temp)
+        for(let i of totalArr[index]){
+            getCombinations(totalArr, index+1, letter+i)
         }
     }
 
-    const subIterate = (element, totalArr, next) => {
-        if(next == totalArr.length-1){
-            return addElements(element, totalArr, next)
-        }
-
-        if(next < totalArr.length){
-            for(let k in totalArr[next]){
-                const temp = element + totalArr[next][k];
-                subIterate(temp, totalArr, next+1)
-            }
-        }
-    }
-
-    const iterate = (totalArr, i) => {
-        for(let k in totalArr[i]){
-            subIterate(totalArr[i][k], totalArr, i+1);
-        }
-    }
-    
-    const operate = (digits) => {
+    const getArr = (digits) => {
         const totalArr = []
-        for(let i of digits){
-            totalArr.push(arr[i])
+        const letter = ''
+        const index = 0
+        const phoneNumberArr = Array.from(digits)
+        for(let i of phoneNumberArr){
+            totalArr.push(Array.from(phoneNumbers[i]))
         }
-        if(totalArr.length == 0){
-            return ""
+        if(totalArr.length){
+            return getCombinations(totalArr, index, letter)
         }
-        if(totalArr.length == 1){
-            return result.push(...totalArr[0])
-        }
-        return iterate(totalArr, 0)
+        
     }
-    operate(digits);
-    return result
+    getArr(digits)
+    return resCombinations
 };
