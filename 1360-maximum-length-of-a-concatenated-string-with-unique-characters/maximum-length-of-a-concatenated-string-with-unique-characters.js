fdda8b7350f37c2e190718ee14c878fbcf1559a0
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-07-11 12:48:04 +0530

    Time: 98 ms (39.13%) | Memory: 55.4 MB (50.93%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.js
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.js
@@ -3,22 +3,26 @@
  * @return {number}
  */
 var maxLength = function(arr) {
-    const checkUnique = (str) => {
-        const charSet = new Set(str);
-        return charSet.size === str.length;
-    }
 
-    const dfs = (index, current) => {
-        if (!checkUnique(current)) return 0;
-        let maxLength = current.length;
-        for (let i = index; i < arr.length; i++) {
-            maxLength = Math.max(maxLength, dfs(i + 1, current + arr[i]));
+    const combinations = (arr) => {
+        const verifyDuplicatesInStr = (str) => {
+            const charSet = new Set(str);
+            return charSet.size == str.length
         }
-        return maxLength;
+        
+        const operate = (index, current) => {
+            if (!verifyDuplicatesInStr(current)){
+                return 0
+            }
+            
+            let max = current.length
+            for(let i = index; i < arr.length; i++){
+                max = Math.max(max, operate(i+1, current+arr[i]))
+            }
+            return max
+        }
+        return operate(0, '')
     }
-
-    return dfs(0, '');
+    
+    return combinations(arr)
 };
-
-// Example usage
-// console.log(maxLength(["ab","cd","cde","cdef","efg","fgh","abxyz"])); // Output: 11
