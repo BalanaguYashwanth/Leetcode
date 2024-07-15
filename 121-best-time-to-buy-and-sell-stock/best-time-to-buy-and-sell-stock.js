527b2b939880591266e05c023eb4e76e4192eb4b
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-07-15 12:19:34 +0530

    Time: 69 ms (80.68%) | Memory: 59.4 MB (23.36%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,16 +2,25 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let result = 0
-    let minimum = prices[0];
-    for(let i=0; i<prices.length; i++){
-        if(prices[i] < minimum){
-            minimum = prices[i]
-        }
-        if(result < (prices[i] - minimum)){
-            result = (prices[i] - minimum)
+var maxProfit = function (prices) {
+    let i = 0;
+    let j = 1;
+    let max = 0;
+
+    const recursion = (i, j) => {
+        while (j < prices.length) {
+            const diff = prices[j] - prices[i];
+            if (diff > max) {
+                max = diff
+            }
+
+            if (prices[i] > prices[j]) {
+                i = j
+            }
+            j++
+
         }
     }
-    return result
+    recursion(i, j)
+    return max
 };
