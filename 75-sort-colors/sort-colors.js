ab0cbd1277d31971270dfa9def6eaf51d2b6acd6
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-07-16 12:51:13 +0530

    Time: 76 ms (5.70%) | Memory: 52.3 MB (5.21%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -2,29 +2,57 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-    let initial = 0
-    let end = nums.length-1
-    let i = 0
-
-    const swap = (leftIndex, rightIndex) => {
-        const temp = nums[leftIndex]
-        nums[leftIndex] = nums[rightIndex]
-        nums[rightIndex] = temp
-    }
-
-    while(i<=end){
-        if(nums[i]==0){
-            swap(initial, i)
-            initial = initial+1
-            i = i+1
-        }
-        else if(nums[i]==1){
-            i = i+1
-        }
-        else{
-            swap(i,end)
-            end = end-1
+var sortColors = function (nums) {
+    const colors = {}
+    const calculateColors = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] in colors) {
+                colors[arr[i]] = colors[arr[i]] + 1
+            } else {
+                colors[arr[i]] = 1
+            }
         }
     }
+    calculateColors(nums)
+
+    const swap = (arr, i, j) => {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    const valiateNextMove = (arr, i, j) => {
+        const temp = arr[i] - 1;
+        const deepTemp = arr[i] - 2;
+        if (temp in colors) {
+            if (colors[temp] > 0) {
+                return sortColors(arr, i, j + 1)
+            }
+        }else if(deepTemp in colors){
+            if (colors[deepTemp] > 0) {
+                return sortColors(arr, i, j + 1)
+            }
+        }
+        colors[arr[i]] = colors[arr[i]] - 1
+        // if (i+1 == j) {
+        //     return sortColors(arr, i + 1, j + 1)
+        // }
+        // return sortColors(arr, i + 1, j)
+        return sortColors(arr, i + 1, i + 2)
+    }
+
+    const sortColors = (arr, i, j) => {
+        if (i < arr.length, j < arr.length) {
+            if (arr[i] > arr[j]) {
+                swap(arr, i, j)
+                valiateNextMove(arr, i, j)
+            }
+            valiateNextMove(arr, i, j)
+        }
+
+    }
+    const i = 0;
+    const j = 1;
+    sortColors(nums, i, j)
+    return nums
 };
