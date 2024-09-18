17ff1b02ee954ea887f75548d5d99f1f33bbb910
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-09-18 16:52:33 +0530

    Time: 207 ms (59.32%) | Memory: 71.7 MB (48.03%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -3,51 +3,55 @@
  * @return {number[]}
  */
 var sortArray = function (nums) {
-    const mergeSort = (arr, len) => {
-        if (arr.length < 2) {
+
+    const operate = (arrs) => {
+        if (arrs.length < 2) {
             return;
         }
-        const mid = Math.round(len / 2);
-        const arr1 = arr.slice(0, mid);
-        const arr2 = arr.slice(mid, arr.length);
-        mergeSort(arr1, arr1.length);
-        mergeSort(arr2, arr2.length);
-        merge(arr, arr1, arr2)
-
+        // console.log('arrs--->', arrs)
+        const mid = Math.ceil(arrs.length / 2)
+        const left = arrs.slice(0, mid);
+        const right = arrs.slice(mid, arrs.length);
+        operate(left)
+        operate(right)
+        // console.log('left--->', left, 'right--->', right)
+        merge(arrs, left, right)
     }
-    mergeSort(nums, nums.length);
+
+    const merge = (parentArr, left, right) => {
+        let leftIndex = 0;
+        let rightIndex = 0;
+        let mainIndex = 0;
+        // console.log('parentArr-->', parentArr, 'left---', left, 'right---', right)
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                parentArr[mainIndex] = left[leftIndex]
+                mainIndex++;
+                leftIndex++;
+            } else {
+                parentArr[mainIndex] = right[rightIndex]
+                mainIndex++;
+                rightIndex++;
+            }
+        }
+
+        while (leftIndex < left.length) {
+            parentArr[mainIndex] = left[leftIndex]
+            mainIndex++;
+            leftIndex++;
+        }
+
+        while (rightIndex < right.length) {
+            parentArr[mainIndex] = right[rightIndex]
+            mainIndex++;
+            rightIndex++;
+        }
+        
+    }
+
+    operate(nums)
     return nums
 };
-
-const merge = (arr, arr1, arr2) => {
-    let leftPointer = 0;
-    let rightPointer = 0;
-    let minPointer = 0;
-    let mid = Math.min(arr1.length, arr2.length);
-
-    while (leftPointer < arr1.length && rightPointer < arr2.length) {
-        if (arr1[leftPointer] < arr2[rightPointer]) {
-            arr[minPointer] = arr1[leftPointer];
-            minPointer++
-            leftPointer++
-        } else {
-            arr[minPointer] = arr2[rightPointer];
-            minPointer++
-            rightPointer++
-        }
-    }
-    for (let i = leftPointer; i < arr1.length; i++) {
-        arr[minPointer] = arr1[i];
-        minPointer++
-    }
-
-    for (let j = rightPointer; j < arr2.length; j++) {
-        arr[minPointer] = arr2[j];
-        minPointer++
-    }
-}
-
-
 
 
 
