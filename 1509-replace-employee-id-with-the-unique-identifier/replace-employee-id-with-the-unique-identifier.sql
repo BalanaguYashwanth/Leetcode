405219f9e1f14113172140faa87d054915561772
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-11-14 12:51:35 +0530

    Time: 1399 ms (40.96%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,11 @@
 # Write your MySQL query statement below
-SELECT EmployeeUNI.unique_id, Employees.name FROM Employees 
-LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id; 
+-- SELECT EmployeeUNI.unique_id, Employees.name FROM Employees 
+-- LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id; 
+
+SELECT CASE 
+WHEN Employees.id IN (SELECT id FROM EmployeeUNI) THEN EmployeeUNI.unique_id
+ELSE null
+END as unique_id, 
+Employees.name
+FROM Employees
+LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
