6831b0add023d865e3de7ed12535d101a0817b8d
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2024-11-14 12:58:11 +0530

    Time: 320 ms (99.45%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,2 @@
--- Write your PostgreSQL query statement below
-SELECT DISTINCT(p1.email)
-FROM Person as p1
-JOIN Person as p2 ON p1.email = p2.email AND p1.id != p2.id
+# Write your MySQL query statement below
+SELECT email FROM Person GROUP BY (email) HAVING COUNT(email) > 1
