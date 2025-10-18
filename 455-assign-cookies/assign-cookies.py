7344ecff3b69d3accb3f3b26dfb625ec5b2dddad
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2025-10-18 15:43:02 +0530

    Time: 35 ms (5.07%) | Memory: 20 MB (27.22%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        child_pointer = 0
+        print('--->',s, '--->', s)
+        for cookie in s:
+            if child_pointer < len(g) and cookie >= g[child_pointer]:
+                child_pointer = child_pointer + 1
+
+        return child_pointer
+
+            
+
+        
