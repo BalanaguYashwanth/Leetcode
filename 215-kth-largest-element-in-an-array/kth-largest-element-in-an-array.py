794e336448fd6a5c9e99544d6846a494d34d6968
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2025-11-08 19:46:16 +0530

    Time: 3722 ms (5.01%) | Memory: 29 MB (14.61%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,23 +1,48 @@
 class Solution:
+    def __init__(self):
+        self.heap = []
+
+    def sort_heap(self, parent_index, child_index):
+        if self.heap[child_index] < self.heap[parent_index]:
+            tmp = self.heap[child_index]
+            self.heap[child_index] = self.heap[parent_index]
+            self.heap[parent_index] = tmp
+
+    def sort_heap_upwards(self, child_index):
+        parent_index = (child_index-1)//2
+        if parent_index >= 0 and self.heap[child_index] < self.heap[parent_index]:
+            self.sort_heap(parent_index, child_index)
+            return self.sort_heap_upwards(parent_index)
+        
+    def perform_min_heap(self, parent_index):
+        left_child_index = (parent_index * 2) + 1
+        right_child_index = (parent_index * 2) + 2
+        if left_child_index < len(self.heap):
+            if right_child_index < len(self.heap):
+                if (self.heap[left_child_index] < self.heap[parent_index] or self.heap[right_child_index] < self.heap[parent_index]):
+                    if self.heap[left_child_index] < self.heap[right_child_index]:
+                        self.sort_heap(parent_index, left_child_index)
+                        return self.perform_min_heap(left_child_index)
+                    else:
+                        self.sort_heap(parent_index, right_child_index)
+                        return self.perform_min_heap(right_child_index)
+            else:
+                if self.heap[left_child_index] < self.heap[parent_index]:
+                    self.sort_heap(parent_index, left_child_index)
+                    return self.perform_min_heap(left_child_index)
+
+
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        arr = []
-        i = 0
+        for index in range(0, len(nums)):
+            self.heap.append(nums[index])
+            self.sort_heap_upwards(child_index=len(self.heap)-1)
 
-        for num in nums:
-            heapq.heappush(arr,num)
-            if len(arr) > k:
-                heapq.heappop(arr)
-
-        return arr[0]
-        # while i < len(nums):
-        #     if arr and nums[i] <= arr[0]:
-        #         arr.insert(0, nums[i])
-        #     else:
-        #         arr.append(nums[i])
-            
-        #     if len(arr) > k:
-        #         arr.pop(0)
-
-        #     i = i + 1
-
-        # return arr[0]
+            if len(self.heap) > k:
+                parent_index = 0
+                self.heap.pop(parent_index)
+                last_child = self.heap[len(self.heap)-1]
+                self.heap.insert(parent_index, last_child)
+                self.heap.pop()
+                self.perform_min_heap(parent_index=parent_index)
+            # print(self.heap)
+        return self.heap[0]
