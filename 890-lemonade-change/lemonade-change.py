047e7f27408e344dea66cbc5fb32fd97e00be107
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2025-11-25 06:32:51 +0530

    Time: 7 ms (30.20%) | Memory: 22.1 MB (8.63%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -9,20 +9,12 @@ class Solution:
             if bill == 5:
                 change_5_dollars = change_5_dollars + 1
             elif bill == 10: 
-                
-                print('--change_5_dollars---', change_5_dollars)
                 if change_5_dollars:
                     change_5_dollars = change_5_dollars - 1
                     change_10_dollars = change_10_dollars + 1
                 else:
-                    print('--bill--', bill)
-                    print('--index---', index)
-                   
-                    print('--change_10_dollars--', change_10_dollars)
                     return False
             elif bill == 20:
-                # print('-change_5_dollars--', change_5_dollars)
-                # print('-change_10_dollars--', change_10_dollars)
                 split_5_dollars = int((bill-5)/5)
                 split_10_dollars = int((bill-5)/10)
                 
@@ -33,9 +25,6 @@ class Solution:
                 elif change_5_dollars >= split_5_dollars:
                     change_5_dollars = change_5_dollars - split_5_dollars
                 else:
-                    print('--index--', index)
-                    print('-else--change_5_dollars--', change_5_dollars)
-                    print('-else--change_10_dollars--', change_10_dollars)
                     return False
                 
         return True
